35421614dc81ec9818f0fd448a40ae296be4e0e5
Ryan Bagan <darcknight1980@gmail.com>
2023-01-30 06:03:54 -0800

    made changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 'use strict';
 
-let seattle = {
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+
+let storeLocation = []
   name: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCookieBought: 6.3,
-  cookiesBought: [],
+  min: 23,
+  max: 65,
+  avgBought: 6.3,
+  randomCustomers: function () {
+    return Math.floor(Math.random() * (this.max - this.min +1) + this.min);
+  }
 };
+
+console.log(seattleStore.randomCustomers());
